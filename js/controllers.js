cc118ac558e8e52d49a3ba3601351be1c8a530f5
alex@alexwlchan.net <alex@alexwlchan.net>
2014-06-12 20:21:56 +0100

    Basic working implementation

--- js/controllers.js
+++ js/controllers.js
@@ -1,4 +1,4 @@
-// 'use strict';
+'use strict';
 
 /* Controllers */
 
@@ -8,24 +8,65 @@ var API_KEY = 'ii4TLRjfMoszcoDkrxBKUk5isHgx0ezQnJ8JWGntYIboVVigez'
 
 tumblrPostsApp.controller('TumblrPostCtrl',
     function($scope, $http) {
-        $scope.hostname = 'example.tumblr.com';
+        $scope.hostname = 'alexwlchan.tumblr.com';
+        
+        $scope.requests_needed = 0;
+        
+        $scope.finished = false;
+        $scope.untagged_exist = false;
 
         $scope.get_meta = function(hostname) {
             console.log('Entered get_meta function')
-            $http({method: 'GET', url: 'http://api.tumblr.com/v2/blog/' + hostname + '/posts?api_key=' + API_KEY}).
+            var API_URL = 'http://api.tumblr.com/v2/blog/' + hostname + '/posts?api_key=' + API_KEY;
+            
+            $http({method: 'GET', url: API_URL}).
                 success(function(data) {
                     $scope.meta = data.meta;
                     $scope.blog = data.response.blog;
-                    console.log(data);
-                    // console.log('Finished the data');
-                    // $scope.myblog = 'fish';
-                    // console.log('fish');
+                    $scope.posts = data.response.posts;
+                    $scope.post_count = $scope.blog.posts;
+                    $scope.requests_needed = Math.floor($scope.post_count / 20);
                 }).error(function(data) {
-                    console.log(data);
                     $scope.meta = data.meta;
                 });
-            console.log('Finished processing data');
+            
+            console.log('Finished get_meta function');
         }
+      
+      $scope.get_more_posts = function(hostname) {
+          var API_URL = 'http://api.tumblr.com/v2/blog/' + hostname + '/posts?api_key=' + API_KEY;
+          for (var i = 1; i <= $scope.requests_needed; i++) {
+              $http.get(API_URL + '&offset=' + (i * 20)).
+                  success(function(data) {
+                      $scope.posts = $scope.posts.concat(data.response.posts);
+                      // console.log(data.response.posts);
+                      // console.log($scope.posts);
+                  });
+              console.log('Getting more posts, at offset ' + (i * 20));
+          }
+          $scope.requests_needed = 0;
+          $scope.finished = true;
+          return 'get more posts function';
+      }
+      
+      
+      // for (var i = 1; i <= $scope.requests_needed; i++) {
+    //       console.log('request from within get_meta ' + (i * 20));
+    //       console.log('before:');
+    //       console.log(posts);
+    //       $http.get(API_URL + '&offset=' + (i * 20)).
+    //           success(function(data) {
+    //               $scope.$apply(function() {
+    //                   $scope.posts.push(data.response.posts);
+    //               });
+    //               console.log(data.response.posts);
+    //           });
+    //           console.log('after:');
+    //           console.log(posts);
+    //           console.log('finished response ' + (i * 20))
+    //   }
+        
+        
         
         // , status, headers, config
         // 
@@ -38,9 +79,14 @@ tumblrPostsApp.controller('TumblrPostCtrl',
         //     })
         // }
         
-        $scope.get_posts = function(hostname, offset, $scope, $http) {
-            return 'cheese';
-        }
+        // $scope.get_posts = function(hostname, offset) {
+        //     // $http({method: 'GET', url: 'http://api.tumblr.com/v2/blog/' + hostname + '/posts?api_key=' + API_KEY}).
+        //   //       success(function(data) {
+        //   //           $scope.posts.push(data.response.posts);
+        //   //       });
+        //   //       console.log($scope.posts);
+        //     return 'cheese';
+        // }
         
     }
 );
